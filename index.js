0d2d5cc3cf23fcfbb897746ddb6062be52bfb653
Jorge Troya <jtm@softtelecom.eu>
2018-02-07 17:16:47 +0100

    passport strategy

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ message: 'hola' });
-});
+passport.use(new googleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken, refreshToke, profile, done) => {
+      console.log('accessToken', accessToken);
+      console.log('profile', profile);
+  })
+  
+);
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
